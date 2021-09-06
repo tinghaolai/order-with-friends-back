1edc9518b5eeb463352cbabc853b429a3eb0eeab
Ting-Hao Lai <analth355@gmail.com>
2021-09-06 21:32:53 +0800

    Validator for menu api

---
+++ src/validator.go
@@ -0,0 +1,18 @@
+package src
+
+import (
+    "github.com/go-playground/validator/v10"
+)
+
+func MultiItemSize(fl validator.FieldLevel) bool {
+    list,_,_,listOk:=fl.GetStructFieldOKAdvanced2(fl.Parent(), "MenuItemList")
+    itemSize,_,_,sizeOk:=fl.GetStructFieldOKAdvanced2(fl.Parent(), "MenuItemSize")
+    if (listOk && sizeOk) {
+        slice,ok:= list.Interface().([]MenuItem)
+        if (ok) {
+            return int64(len(slice)) == itemSize.Int()
+        }
+    }
+
+    return false
+}
